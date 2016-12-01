6134818a9c02baae55e4bf75e0f024aa731ba2b6
Gisellabella <gisella@gmail.com>
2016-12-01 12:25:25 -0700

    first commit

---
+++ models/book.js
@@ -0,0 +1,16 @@
+// book.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+	title:  String,
+	author: String,
+	image: String,
+	releaseDate: Date
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+
+module.exports = Book;
+
